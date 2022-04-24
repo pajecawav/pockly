58aac733c5aff0cfc1000a8ec6fad79cebbe3bf5
pajecawav <pajecawav@gmail.com>
2022-04-24 15:24:00 +0300

    remove prisma client ci hack

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,13 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import isCi from "is-ci";
 
-// HACK: This module is imported during graphql types generation in CI where no
-// `DATABASE_URL` env variable is specified so the prisma client throws an
-// error and stops generation script from working. To avoid this we detect CI
-// environment and avoid instantiating PrismaClient in those.
-export const db = isCi
-	? (null as any as PrismaClient)
-	: ((global as any).db as PrismaClient) || new PrismaClient();
+export const db = ((global as any).db as PrismaClient) || new PrismaClient();
 
 // https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
 if (process.env.NODE_ENV !== "production") {
