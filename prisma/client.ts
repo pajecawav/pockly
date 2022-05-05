a210ba6c2188e2725072cc2c1f8374d8769d7e18
pajecawav <pajecawav@gmail.com>
2022-05-05 20:13:04 +0300

    add ci

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = ((global as any).db as PrismaClient) || new PrismaClient();
-
 // https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
-if (process.env.NODE_ENV !== "production") {
-	(global as any).db = db;
+declare global {
+	var db: PrismaClient | undefined;
 }
+
+const prisma = global.db || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+	global.db = prisma;
+}
+
+export const db = prisma;
