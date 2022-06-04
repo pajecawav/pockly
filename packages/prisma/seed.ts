37b7473214deab645ab9e8f7a4e5d4a8247d69c7
pajecawav <pajecawav@gmail.com>
2022-06-04 21:41:19 +0300

    add db scripts

--- packages/prisma/seed.ts
+++ packages/prisma/seed.ts
@@ -10,6 +10,7 @@ async function main() {
 		throw new Error("You have to create a user before seeding database");
 	}
 
+	// random bookmarks
 	await db.bookmark.createMany({
 		data: Array.from({ length: 40 }, () => ({
 			userId: user.id,
@@ -17,6 +18,60 @@ async function main() {
 			title: faker.lorem.sentence(),
 		})),
 	});
+
+	const predefinedBookmarks: {
+		url: string;
+		title: string;
+		tags?: string[];
+	}[] = [
+		{
+			url: "https://www.youtube.com",
+			title: "YouTube",
+			tags: ["youtube", "foo"],
+		},
+		{
+			url: "https://www.notion.so",
+			title: "Notion – One workspace. Every team.",
+			tags: ["notion", "bar"],
+		},
+		{
+			url: "https://linear.app",
+			title: "Linear – The issue tracking tool you’ll enjoy using.",
+			tags: ["foo", "linear"],
+		},
+		{
+			url: "https://www.prisma.io",
+			title: "Prisma - Next-generation Node.js and TypeScript ORM for Databases",
+			tags: ["bar", "prisma"],
+		},
+		{
+			url: "https://google.com",
+			title: "Google",
+			tags: ["bar", "foo", "google"],
+		},
+	];
+
+	// pre-defined bookmarks
+	for (const bookmark of predefinedBookmarks) {
+		await db.bookmark.create({
+			data: {
+				userId: user.id,
+				url: bookmark.url,
+				title: bookmark.title,
+				tags: {
+					connectOrCreate: bookmark.tags?.map(tag => ({
+						create: {
+							name: tag,
+							userId: user.id,
+						},
+						where: {
+							name_userId: { name: tag, userId: user.id },
+						},
+					})),
+				},
+			},
+		});
+	}
 }
 
 main()
