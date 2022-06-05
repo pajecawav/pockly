a822c2f6ae960bf1246578e05982eb4ab8ce0276
pajecawav <pajecawav@gmail.com>
2022-06-05 23:52:00 +0300

    show animation after deleting bookmark

--- packages/extension/src/popup/components/BookmarkSaved.tsx
+++ packages/extension/src/popup/components/BookmarkSaved.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Logo } from "@/components/Logo";
 import { Textarea } from "@/components/Textarea";
+import { cn } from "@/utils";
 import { FormEvent, useState } from "react";
 import { BOOKMARK_PAGE_BASE_URL, READING_LIST_URL } from "../config";
 import { Bookmark, deleteBookmark, updateBookmark } from "../mutations";
@@ -25,8 +26,6 @@ export function BookmarkSaved({ bookmark }: Props) {
 
 		setIsSaving(true);
 
-		await new Promise(r => setTimeout(r, 5000));
-
 		const updated = await updateBookmark({ id: bookmark.id, title, note });
 		setTitle(updated.title);
 		setNote(updated.note ?? "");
@@ -36,74 +35,82 @@ export function BookmarkSaved({ bookmark }: Props) {
 
 	async function handleDelete() {
 		setIsDeleting(true);
-		await new Promise(r => setTimeout(r, 5000));
 		await deleteBookmark(bookmark.id);
 		setDeleted(true);
 	}
 
+	const headerText = deleted
+		? "Deleted Bookmark"
+		: isDeleting
+		? "Deleting Bookmark..."
+		: "Saved Bookmark";
+
 	return (
-		<div className="flex-grow flex flex-col gap-2">
+		<div className="flex-grow flex flex-col">
 			<div className="flex items-center gap-1 text-xl">
 				<Logo className="h-[1em] w-[1em] text-sky-600" />{" "}
-				<span>{deleted ? "Deleted bookmark" : "Saved to Pockly"}</span>
+				<span>{headerText}</span>
 			</div>
 
-			{!deleted && (
-				<>
-					<div className="text-sm">
-						<a
-							className="text-blue-500 hover:underline"
-							href={READING_LIST_URL}
-						>
-							Reading List
-						</a>
-						<span className="text-black"> | </span>
-						<a
-							className="text-blue-500 hover:underline"
-							href={`${BOOKMARK_PAGE_BASE_URL}/${bookmark.id}`}
-						>
-							Bookmark Page
-						</a>
-					</div>
-
-					<form
-						className="flex-grow flex flex-col gap-2"
-						onSubmit={handleSubmit}
+			<div
+				className={cn(
+					"flex-grow flex flex-col gap-2 transition-opacity duration-500",
+					deleted && "opacity-0"
+				)}
+			>
+				<div className="text-sm">
+					<a
+						className="text-blue-500 hover:underline"
+						href={READING_LIST_URL}
 					>
-						<Input
-							placeholder="Title"
-							value={title}
-							onChange={e => setTitle(e.target.value)}
-						/>
+						Reading List
+					</a>
+					<span className="text-black"> | </span>
+					<a
+						className="text-blue-500 hover:underline"
+						href={`${BOOKMARK_PAGE_BASE_URL}/${bookmark.id}`}
+					>
+						Bookmark Page
+					</a>
+				</div>
 
-						<Textarea
-							className="resize-none flex-grow"
-							placeholder="Write your notes here"
-							value={note}
-							onChange={e => setNote(e.target.value)}
-						/>
+				<form
+					className="flex-grow flex flex-col gap-2"
+					onSubmit={handleSubmit}
+				>
+					<Input
+						placeholder="Title"
+						value={title}
+						onChange={e => setTitle(e.target.value)}
+					/>
 
-						<div className="self-center flex gap-2">
-							<Button
-								type="submit"
-								isLoading={isSaving}
-								disabled={isProcessing}
-							>
-								Update
-							</Button>
-							<Button
-								type="button"
-								intent="danger"
-								isLoading={isDeleting}
-								disabled={isProcessing}
-								onClick={handleDelete}
-							>
-								Delete
-							</Button>
-						</div>
-					</form>
-				</>
-			)}
+					<Textarea
+						className="resize-none flex-grow"
+						placeholder="Write your notes here"
+						value={note}
+						onChange={e => setNote(e.target.value)}
+					/>
+
+					<div className="self-center flex gap-2">
+						<Button
+							type="submit"
+							isLoading={isSaving}
+							disabled={isProcessing}
+						>
+							Update
+						</Button>
+						<Button
+							type="button"
+							intent="danger"
+							isLoading={isDeleting}
+							disabled={isProcessing}
+							onClick={handleDelete}
+						>
+							Delete
+						</Button>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 }
